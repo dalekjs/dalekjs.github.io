3d21e4edb99b604c3f27f13eaa94b83979ffbb88
Drone Build <support@drone.io>
2013-07-19 14:43:11 +0000

    Updating gh-pages from dalek-browser-ie

---
+++ package/dalek-browser-ie/master/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Browser.InternetExplorer"
+    ],
+    "modules": [
+        "DalekJS"
+    ],
+    "allModules": [
+        {
+            "displayName": "DalekJS",
+            "name": "DalekJS",
+            "description": "Internet Explorer Driver base class"
+        }
+    ]
+} };
+});
---
+++ package/dalek-browser-ie/master/complexity/files/index_js/report.history.js
@@ -0,0 +1,1 @@
+__history = [{"date":"Fri, 19 Jul 2013 14:43:09 GMT","sloc":222,"lloc":73,"functions":13,"deliveredBugs":1.0112929462777493,"maintainability":71.78514980517332,"lintErrors":0,"difficulty":33.1875}]
