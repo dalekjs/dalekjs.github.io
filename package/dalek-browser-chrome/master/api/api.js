97e925c8e51986996f1e2355cff045cfc0a8b6b1
Drone Build <support@drone.io>
2013-07-19 15:40:05 +0000

    Updating gh-pages from dalek-browser-chrome

---
+++ package/dalek-browser-chrome/master/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Browser.ChromeDriver"
+    ],
+    "modules": [
+        "DalekJS"
+    ],
+    "allModules": [
+        {
+            "displayName": "DalekJS",
+            "name": "DalekJS",
+            "description": "Chrome Driver base class"
+        }
+    ]
+} };
+});
