16d9f9881dddd595eaabaf503dfe6e200a2a08fc
Drone Build <support@drone.io>
2013-07-19 16:05:45 +0000

    Updating gh-pages from dalek-browser-phantomjs

---
+++ package/dalek-browser-phantomjs/master/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Browser.PhantomJSDriver"
+    ],
+    "modules": [
+        "DalekJS"
+    ],
+    "allModules": [
+        {
+            "displayName": "DalekJS",
+            "name": "DalekJS",
+            "description": "PhantomJS Driver base class"
+        }
+    ]
+} };
+});
