c804c80302df9ee4ab1398f06937163b20e06740
Drone Build <support@drone.io>
2013-07-22 16:11:56 +0000

    Updating gh-pages from dalek-internal-test

---
+++ package/dalek-internal-test/master/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Dalek.Test"
+    ],
+    "modules": [
+        "DalekJS"
+    ],
+    "allModules": [
+        {
+            "displayName": "DalekJS",
+            "name": "DalekJS",
+            "description": "Generates an test instance"
+        }
+    ]
+} };
+});
