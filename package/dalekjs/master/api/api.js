cfc30fbba3d6711001f9f7d3f6cb87b5e2e45f51
Drone Build <support@drone.io>
2013-07-22 15:30:14 +0000

    Updating gh-pages from dalekjs

---
+++ package/dalekjs/master/api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Dalek"
+    ],
+    "modules": [
+        "DalekJS"
+    ],
+    "allModules": [
+        {
+            "displayName": "DalekJS",
+            "name": "DalekJS",
+            "description": "Daleks base module\nUsed to configure all the things\nand to start off the tests"
+        }
+    ]
+} };
+});
