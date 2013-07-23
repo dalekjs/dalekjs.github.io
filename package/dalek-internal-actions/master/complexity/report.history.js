ce0abb8ba9e8d27d08afe0e420fa180b2085f564
Drone Build <support@drone.io>
2013-07-23 18:40:51 +0000

    Updating gh-pages from dalek-internal-actions

--- package/dalek-internal-actions/master/complexity/report.history.js
+++ package/dalek-internal-actions/master/complexity/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 22 Jul 2013 18:14:17 GMT","total":{"sloc":707,"maintainability":72.79837683812791},"average":{"sloc":707,"maintainability":"72.80"}}]
+__history = [{"date":"Tue, 23 Jul 2013 18:40:45 GMT","total":{"sloc":716,"maintainability":72.79837683812791},"average":{"sloc":716,"maintainability":"72.80"}}]
