01de7b6cfe914a2b386c979d6abc1a87b2760428
Drone Build <support@drone.io>
2013-07-29 20:32:04 +0000

    Updating gh-pages from dalek-reporter-html

---
+++ package/dalek-reporter-html/master/api/api.js
@@ -0,0 +1,16 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Reporter"
+    ],
+    "modules": [
+        "Reporter"
+    ],
+    "allModules": [
+        {
+            "displayName": "Reporter",
+            "name": "Reporter"
+        }
+    ]
+} };
+});
