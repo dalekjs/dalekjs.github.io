c5667dc7de977f49d50181a9516a7d2e0e7bf36e
Drone Build <support@drone.io>
2013-08-01 16:42:28 +0000

    Updating gh-pages from dalek-browser-phantomjs

--- package/dalek-browser-phantomjs/master/api/api.js
+++ package/dalek-browser-phantomjs/master/api/api.js
@@ -10,7 +10,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "DalekJS",
             "name": "DalekJS",
-            "description": "PhantomJS Driver base class"
+            "description": "This module is a browser plugin for [DalekJS](//github.com/dalekjs/dalek).\nIt provides a browser launcher as well the PhantomJS browser itself.\n\nThe browser plugin comes bundled with the DalekJS base framework.\n\nYou can use the browser plugin beside others (it is the default)\nby adding a config option to the your Dalekfile:\n\n```javascript\n\"browsers\": [\"phantomjs\", \"chrome\"]\n```\n\nOr you can tell Dalek that it should test in this & another browser via the command line:\n\n```bash\n$ dalek mytest.js -b phantomjs,chrome\n```"
         }
     ]
 } };
