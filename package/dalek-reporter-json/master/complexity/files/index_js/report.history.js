a0726b1a0c8adf20a85b8c10cbbacfa5b9016001
Drone Build <support@drone.io>
2013-08-20 18:09:21 +0000

    Updating gh-pages from dalek-reporter-json

--- package/dalek-reporter-json/master/complexity/files/index_js/report.history.js
+++ package/dalek-reporter-json/master/complexity/files/index_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 30 Jul 2013 12:12:14 GMT","sloc":246,"lloc":77,"functions":10,"deliveredBugs":1.1751434393970386,"maintainability":67.64412001145087,"lintErrors":0,"difficulty":33.837209302325576}]
+__history = [{"date":"Tue, 20 Aug 2013 18:09:10 GMT","sloc":248,"lloc":78,"functions":10,"deliveredBugs":1.1885992803061651,"maintainability":67.45190535672687,"lintErrors":0,"difficulty":34.18604651162791}]
