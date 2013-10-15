1440e05450c35fb2751838d530aa087f7ccf30aa
Drone Build <support@drone.io>
2013-10-15 16:15:44 +0000

    Updating gh-pages from dalek-reporter-console

--- package/dalek-reporter-console/master/complexity/files/lib_loglevel_level2_js/report.history.js
+++ package/dalek-reporter-console/master/complexity/files/lib_loglevel_level2_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 15 Oct 2013 15:50:28 GMT","sloc":116,"lloc":100,"functions":5,"deliveredBugs":1.0547385562036973,"maintainability":55.07567588009742,"lintErrors":0,"difficulty":34.9375}]
+__history = [{"date":"Tue, 15 Oct 2013 16:15:28 GMT","sloc":116,"lloc":100,"functions":5,"deliveredBugs":1.0547385562036973,"maintainability":55.07567588009742,"lintErrors":0,"difficulty":34.9375}]
--- package/dalek-reporter-console/master/complexity/files/lib_loglevel_level5_js/report.history.js
+++ package/dalek-reporter-console/master/complexity/files/lib_loglevel_level5_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 15 Oct 2013 15:50:28 GMT","sloc":32,"lloc":16,"functions":1,"deliveredBugs":0.14519266120527252,"maintainability":62.71625425179364,"lintErrors":0,"difficulty":7.448275862068965}]
+__history = [{"date":"Tue, 15 Oct 2013 16:15:28 GMT","sloc":32,"lloc":16,"functions":1,"deliveredBugs":0.14519266120527252,"maintainability":62.71625425179364,"lintErrors":0,"difficulty":7.448275862068965}]
