81cc6fa830692ad462bdeacb644996a8fbdaafb1
Drone Build <support@drone.io>
2013-10-15 19:56:29 +0000

    Updating gh-pages from dalek-driver-sauce

---
+++ package/dalek-driver-sauce/master/api/api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Dalek.Browser",
+        "Dalek.DriverNative.Commands.Cookie",
+        "Dalek.DriverNative.Commands.Element",
+        "Dalek.DriverNative.Commands.Execute",
+        "Dalek.DriverNative.Commands.Frame",
+        "Dalek.DriverNative.Commands.Page",
+        "Dalek.DriverNative.Commands.Screenshot",
+        "Dalek.DriverNative.Commands.Url",
+        "Dalek.DriverNative.Commands.Window",
+        "Dalek.Sauce"
+    ],
+    "modules": [
+        "Driver"
+    ],
+    "allModules": [
+        {
+            "displayName": "Driver",
+            "name": "Driver",
+            "description": "Cookie related methods"
+        }
+    ]
+} };
+});
