609f53e8f44005c7ddcf9bcdca25b4bfc1ee077f
Drone Build <support@drone.io>
2013-10-17 12:59:20 +0000

    Updating gh-pages from dalekjs

--- package/dalekjs/master/complexity/report.history.js
+++ package/dalekjs/master/complexity/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 16 Oct 2013 19:16:35 GMT","total":{"sloc":299,"maintainability":70.87395329565175},"average":{"sloc":299,"maintainability":"70.87"}}]
+__history = [{"date":"Thu, 17 Oct 2013 12:59:06 GMT","total":{"sloc":299,"maintainability":70.87395329565175},"average":{"sloc":299,"maintainability":"70.87"}}]
