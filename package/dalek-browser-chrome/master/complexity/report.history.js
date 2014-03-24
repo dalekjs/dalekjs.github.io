63618a0ec8bae30cabdbecd789756e7e60813c13
Drone Build <support@drone.io>
2014-03-24 17:36:37 +0000

    Updating gh-pages from dalek-browser-chrome

--- package/dalek-browser-chrome/master/complexity/report.history.js
+++ package/dalek-browser-chrome/master/complexity/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Mon, 10 Mar 2014 18:11:37 GMT","total":{"sloc":1015,"maintainability":215.2407466779474},"average":{"sloc":338,"maintainability":"71.75"}}]
+__history = [{"date":"Mon, 24 Mar 2014 17:36:24 GMT","total":{"sloc":1015,"maintainability":215.2407466779474},"average":{"sloc":338,"maintainability":"71.75"}}]
