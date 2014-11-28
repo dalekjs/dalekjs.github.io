3b80203f9fb344ecb1937aedf0eb1a1d8d4cb419
Drone Build <support@drone.io>
2014-11-28 15:08:14 +0000

    Updating gh-pages from dalekjs

--- package/dalekjs/master/complexity/files/_reporter_js/report.history.js
+++ package/dalekjs/master/complexity/files/_reporter_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Fri, 28 Nov 2014 14:27:49 GMT","sloc":62,"lloc":17,"functions":2,"deliveredBugs":0.12430789508874915,"maintainability":71.33251447098549,"lintErrors":[],"difficulty":12.157894736842106}]
+__history = [{"date":"Fri, 28 Nov 2014 15:08:07 GMT","sloc":62,"lloc":17,"functions":2,"deliveredBugs":0.12430789508874915,"maintainability":71.33251447098549,"lintErrors":[],"difficulty":12.157894736842106}]
--- package/dalekjs/master/complexity/files/_timer_js/report.history.js
+++ package/dalekjs/master/complexity/files/_timer_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Fri, 28 Nov 2014 14:27:49 GMT","sloc":97,"lloc":31,"functions":5,"deliveredBugs":0.2929825799709793,"maintainability":70.75368074527475,"lintErrors":[],"difficulty":24.72222222222222}]
+__history = [{"date":"Fri, 28 Nov 2014 15:08:07 GMT","sloc":97,"lloc":31,"functions":5,"deliveredBugs":0.2929825799709793,"maintainability":70.75368074527475,"lintErrors":[],"difficulty":24.72222222222222}]
